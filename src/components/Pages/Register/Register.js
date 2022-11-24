7e13354fa9f6c8012d202df97466f44300bb4fa0
ajkn1904 <ajknishat@gmail.com>
2022-11-24 17:48:45 +0600

    registration done

--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -1,9 +1,163 @@
-import React from 'react';
+import { async } from '@firebase/util';
+import { warning } from 'daisyui/src/colors/colorNames';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast, Toaster } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/AuthProvider';
 
 const Register = () => {
+
+    const { createUser, continueWithProvider,
+        userProfileUpdate, loading } = useContext(AuthContext);
+    const { register, formState: { errors }, handleSubmit } = useForm()
+    const [registerError, setRegisterError] = useState('')
+
+    if (loading) {
+        return <p>loading..........</p>
+    }
+
+
+    const storeUser = (name, email, role) => {
+        const user ={
+            name,
+            email,
+            role
+        }
+        fetch('http://localhost:5000/users',{ 
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        
+    })
+    }
+    const handleProfile = (data) => {
+        const userInfo = {
+            displayName: data.name
+        }
+
+        userProfileUpdate(userInfo)
+        .then(() => {
+            console.log(data.name, data.email, data.role)
+            storeUser(data.name, data.email, data.role)
+        })
+        .catch((error) => setRegisterError(error.message))
+    }
+
+
+    const handleRegister = data => {
+        if (data.password !== data.confirmPassword) {
+            setRegisterError('Your password did not match.')
+            return setRegisterError;
+        }
+
+        setRegisterError('')
+        createUser(data.email, data.password)
+            .then(res => {
+                const user = res.user
+                console.log(user)
+                toast.success('Registration Successful')
+                handleProfile(data)
+            }
+            )
+
+            .catch(error => setRegisterError(error.message))
+
+    }
+
+
     return (
-        <div>
-           this is register 
+        <div className='flex justify-center items-center my-20 p-4'>
+            <div className='card shadow-xl w-96 p-7'>
+                <h1 className='text-xl font-bold text-center my-10'>Register Now</h1>
+
+                <form onSubmit={handleSubmit(handleRegister)}>
+
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" className="w-full my-3 input input-bordered"  {...register("name")} />
+
+
+
+                    <label className="label">
+                        <span className="label-text">Select Account Type</span>
+                    </label>
+                    <select className="select select-bordered w-full max-w-xs"  {...register("role")} >
+                        <option value={'Normal User'}>Normal User</option>
+                        <option value={'Seller'}>Seller</option>
+                    </select>
+
+
+
+
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" className="w-full my-3 input input-bordered"  {...register("email", { required: "Email is required" })} />
+
+                    {errors.email && <p className='text-error'>{errors.email?.message}</p>}
+
+
+
+                    <label>
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" className="w-full my-3 input input-bordered"  {...register("password",
+                        {
+                            required: "Password is required",
+                            minLength: { value: 6, message: 'Password must be at least 6 character long' },
+                            pattern: {
+                                value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                                message: 'Password must contain at least an uppercase, a special character and a number.'
+                            }
+                        })} />
+
+                    {errors.password && <p className='text-error'>{errors.password?.message}</p>}
+
+
+
+                    <label>
+                        <span className="label-text">Re-type Password</span>
+                    </label>
+                    <input type="password" className="w-full my-3 input input-bordered"  {...register("confirmPassword",
+                        {
+                            required: "Password is required",
+                            minLength: { value: 6, message: 'Password must be at least 6 character long' }
+                        })} />
+
+                    {errors.confirmPassword && <p className='text-error'>{errors.confirmPassword?.message}</p>}
+
+
+
+                    <label className="label">
+                        <span className="label-text">Forget Password?</span>
+                    </label>
+                    <small className='text-red-600'>{registerError}</small>
+
+
+
+                    <div className="flex flex-col w-full border-opacity-50">
+
+                        <button className="w-full my-3 btn text-white" type="submit">Register</button>
+                        <small>Already have an account? <Link to="/login" className='text-primary'>Login now</Link></small>
+
+                        <div className="divider">OR</div>
+
+                        <button className="w-full my-3 btn btn-outline btn-primary" type="submit">CONTINUE WITH GOOGLE</button>
+
+                    </div>
+
+                </form>
+
+            </div>
+            <Toaster></Toaster>
         </div>
     );
 };
