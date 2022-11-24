b50970c15cc80665d2d1334374a78d7587b5a401
ajkn1904 <ajknishat@gmail.com>
2022-11-24 06:07:25 +0600

    firebase & authcontext added

---
+++ src/components/Contexts/AuthProvider.js
@@ -0,0 +1,73 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth'
+import app from '../../Firebase/firebase.config';
+
+export const AuthContext = createContext();
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+
+    //for creating new user with email and password
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    //for login with email and password
+    const userLogin = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    //for login or create new user with any provider like google, github or so on
+    const continueWithProvider = (provider) => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    };
+
+    //for update the user info on firebase 
+    const userProfileUpdate = userInfo => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, userInfo);
+    };
+
+    //for logout
+    const userLogout = () => {
+        setLoading(true);
+        return signOut(auth)
+    }
+
+
+    //useEffect for tracking a specific user for the site
+    useEffect(() => {
+        const unSubscriber = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => unSubscriber();
+    }, [])
+
+
+
+    const authInfo = {
+        user,
+        createUser,
+        userLogin,
+        continueWithProvider,
+        userProfileUpdate,
+        userLogout,
+        loading
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
