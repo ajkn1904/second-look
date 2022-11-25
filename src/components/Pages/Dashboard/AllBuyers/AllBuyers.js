9d0e96eefb616016493c20ae85435d22d81fc216
ajkn1904 <ajknishat@gmail.com>
2022-11-25 12:08:44 +0600

    all buyers and sellers loaded

--- src/components/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/components/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import axios from 'axios';
 
-const AllBuyers = () => {
-    return (
-        <div>
-            buyers
-        </div>
-    );
-};
+export default class AllBuyers extends React.Component {
+    state = {
+        buyers: []
+      }
 
-export default AllBuyers;
+      componentDidMount() {
+        axios.get('http://localhost:5000/buyers')
+          .then(res => {
+            const buyers = res.data;
+            this.setState({ buyers });
+          })
+      }
+      render() {
+        return (
+            <div className='p-5 sm:p-5 md:p-8 lg:p-14 my-5'>
+                <h2 className='text-2xl my-4 font-bold text-center'>ALL BUYERS</h2>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+    
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Make Admin</th>
+                                <th>Delete</th>
+    
+                            </tr>
+                        </thead>
+                        <tbody>
+    
+                            {this.state.buyers &&
+                                this.state.buyers?.map((buyer, i) => <tr key={buyer._id} className="hover">
+                                    <th>{[i + 1]}</th>
+                                        <td>{buyer.name}</td>
+                                        <td>{buyer.email}</td>
+                                        <td><button className='btn btn-success'>Pay</button></td>
+                                        <td><button className='btn btn-error'>Delete</button></td>
+                                </tr>
+    
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    };
+  }
--- src/components/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/components/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,11 +1,52 @@
+import axios from 'axios';
 import React from 'react';
 
-const AllSellers = () => {
-    return (
-        <div>
-            All Sellers
-        </div>
-    );
-};
+export default class AllSellers extends React.Component {
+    state = {
+        sellers: []
+      }
 
-export default AllSellers;
+      componentDidMount() {
+        axios.get('http://localhost:5000/sellers')
+          .then(res => {
+            const sellers = res.data;
+            this.setState({ sellers });
+          })
+      }
+      render() {
+        return (
+            <div className='p-5 sm:p-5 md:p-8 lg:p-14 my-5'>
+                <h2 className='text-2xl my-4 font-bold text-center'>ALL SELLERS</h2>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+    
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Make Admin</th>
+                                <th>Delete</th>
+    
+                            </tr>
+                        </thead>
+                        <tbody>
+    
+                            {this.state.sellers &&
+                                this.state.sellers?.map((seller, i) => <tr key={seller._id} className="hover">
+                                    <th>{[i + 1]}</th>
+                                        <td>{seller.name}</td>
+                                        <td>{seller.email}</td>
+                                        <td><button className='btn btn-success'>Pay</button></td>
+                                        <td><button className='btn btn-error'>Delete</button></td>
+                                </tr>
+    
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    };
+  }
