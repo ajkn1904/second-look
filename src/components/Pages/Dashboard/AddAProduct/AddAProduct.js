4f836acf42cdea7593548320c693c3e69e5caf34
ajkn1904 <ajknishat@gmail.com>
2022-11-26 05:16:48 +0600

    add product and my products route done

--- src/components/Pages/Dashboard/AddAProduct/AddAProduct.js
+++ src/components/Pages/Dashboard/AddAProduct/AddAProduct.js
@@ -1,10 +1,205 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast, Toaster } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../Contexts/AuthProvider';
+import LoadingSpinner from '../../../Shared/LoadingSpinner/LoadingSpinner';
 
 const AddAProduct = () => {
+
+    const date = new Date();
+
+    const { user } = useContext(AuthContext)
+    const { register, formState: { errors }, handleSubmit } = useForm()
+    const navigate = useNavigate()
+    const imgHostingKey = process.env.REACT_APP_imgbb_key;
+
+
+    const { data: category = [], isLoading } = useQuery({
+        queryKey: ['category'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/category')
+            const data = res.json()
+            return data
+        }
+    })
+
+    if (isLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+
+    const handleAddProduct = (data) => {
+
+        const image = data.image[0]
+        const formData = new FormData()
+        formData.append('image', image)
+        const url = `https://api.imgbb.com/1/upload?&key=${imgHostingKey}`
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if (imgData.success) {
+                    //console.log(imgData.data.url)
+
+                    const product = {
+                        cat_id: data.cat_id,
+                        name: data.name,
+                        image: imgData.data.url,
+                        description: data.description,
+                        location: data.location,
+                        originalPrice: data.originalPrice,
+                        resalePrice: data.resalePrice,
+                        used: data.used,
+                        sellerName: data.sellerName,
+                        sellerEmail: data.sellerEmail,
+                        conditionType: data.conditionType,
+                        phone: data.phone,
+                        postingTime: date.toDateString(),
+                        purchaseYr: data.purchaseYr,
+                        status: 'available',
+                    }
+
+                    //console.log(product)
+                    fetch('http://localhost:5000/product', {
+                        method: 'POST',
+                        headers: {
+                            "content-type": "application/json"
+                        },
+                        body: JSON.stringify(product)
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data)
+                            toast.success('Your product added Successfully')
+                            navigate('/dashboard/myProducts')
+                        })
+                }
+            })
+
+
+    }
+
+
     return (
-        <div>
-            add products
-        </div>
+        <>
+
+            <h1 className='text-2xl font-bold text-center mt-10 mb-4'>ADD A PRODUCT</h1>
+            <div className='flex justify-center items-center mb-20 p-4'>
+                <div className='card shadow-xl w-8/12 bg-slate-100 p-7'>
+
+                    <form onSubmit={handleSubmit(handleAddProduct)}>
+
+                        <label className="label">
+                            <span className="label-text">Name of the book</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("name")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Select Category</span>
+                        </label>
+                        <select className="select select-bordered w-full max-w-xs"  {...register("cat_id")} >
+                            {
+                                category.map(item =>
+
+                                    <option key={item._id} value={item._id}>{item.name}</option>
+
+                                )
+                            }
+                        </select>
+
+
+                        <label className="label">
+                            <span className="label-text">Book's image</span>
+                        </label>
+                        <input type="file" className="w-full my-3 input input-bordered"  {...register("image", {
+                            required: 'Photo is required'
+                        })} />
+
+                        {errors.image && <p className='text-error'>{errors.image?.message}</p>}
+
+
+                        <label className="label">
+                            <span className="label-text">Description</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("description")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Location</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("location")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Original Price</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("originalPrice")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Resale Price</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("resalePrice")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Year of purchase</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("purchaseYr")} />
+
+
+
+                        <label className="label">
+                            <span className="label-text">Years of Used</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("used")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Condition type(excellent, good, fair)</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("conditionType")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Phone Number</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered"  {...register("phone")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Name of the seller</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered" defaultValue={user.displayName} readOnly {...register("sellerName")} />
+
+
+                        <label className="label">
+                            <span className="label-text">Email of the seller</span>
+                        </label>
+                        <input type="text" className="w-full my-3 input input-bordered" defaultValue={user.email}  {...register("sellerEmail")} readOnly />
+
+
+                        <div className="flex flex-col w-full border-opacity-50">
+
+                            <button className="w-full my-3 btn text-white" type="submit">Add Product</button>
+
+
+
+                        </div>
+
+                    </form>
+                    
+                </div>
+
+            </div>
+
+
+        </>
     );
 };
 
--- src/components/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/components/Pages/Dashboard/MyProducts/MyProducts.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { AuthContext } from '../../../Contexts/AuthProvider';
+import LoadingSpinner from '../../../Shared/LoadingSpinner/LoadingSpinner';
 
 const MyProducts = () => {
+    const { user, loading } = useContext(AuthContext)
+
+    const url = `http://localhost:5000/products?email=${user?.email}`
+
+
+    const { data = [], isLoading } = useQuery({
+        queryKey: ['products', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url, {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+            const data = await res.json()
+            return data
+        }
+    })
+
+    if (loading || isLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+
     return (
-        <div>
-            my products
+        <div className='p-5 sm:p-5 md:p-8 lg:p-14 my-5'>
+            <h2 className='text-2xl my-4 font-bold text-center'>MY PRODUCTS</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Image</th>
+                            <th className='w-3'>Title</th>
+                            <th>Original Price</th>
+                            <th>Resale Price</th>
+                            <th>Status</th>
+                            <th>Advertise</th>
+                            <th>Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {data &&
+                            data?.map((item, i) => <tr key={item._id} className="hover">
+                                <th>{[i + 1]}</th>
+                                <td className="avatar">
+                                    <div className="w-16 rounded">
+                                        <img src={item.image} alt="" />
+                                    </div>
+                                    </td>
+                                    <td className='w-3'>{item.name}</td>
+                                    <td>${item.originalPrice}</td>
+                                    <td>${item.resalePrice}</td>
+                                    <td className='font-semibold uppercase bg-green-100'>{item.status}</td>
+                                    <td><button className='btn btn-warning btn-sm'>Advertise</button></td>
+                                    <td><button className='btn btn-error btn-sm'>Delete</button></td>
+                            </tr>
+
+                            )
+                        }
+                    </tbody>
+                </table>
+            <Toaster></Toaster>
+            </div>
+
+            
         </div>
     );
 };
