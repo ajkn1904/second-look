3c80601d0d4ecd7d82d15b8f886da17f459a0bd8
ajkn1904 <ajknishat@gmail.com>
2022-11-26 07:37:22 +0600

    deleting user functionality added

--- src/components/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/components/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,21 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import { toast } from 'react-hot-toast';
 
 const AllBuyers = () => {
     const [data, setData] = useState([]);
+    const [reFetch, setReFetch] = useState(false);
 
-    useEffect(() => {
-        getBuyers();
-    }, []);
+
     //using axios to get data from api
-    const getBuyers = () => {
+    useEffect(() => {
         axios.get('http://localhost:5000/buyers')
             .then(res => {
                 const buyers = res.data;
                 setData(buyers)
+                //console.log(buyers)
             })
             .catch(error => console.log(error));
+    }, [reFetch]);
+
+
+    //console.log(data)
+
+    const handleDelete = (id) => {
+        const doDelete = window.confirm('Do you want to delete this buyer?');
+        if (doDelete) {
+            fetch(`http://localhost:5000/buyers/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount === 1) {
+                        setReFetch(!reFetch)
+                        toast.error("Deleted Successfully")
+                    }
+                })
+        }
     }
+
+
     return (
         <div className='p-5 sm:p-5 md:p-8 lg:p-14 my-5'>
             <h2 className='text-2xl my-4 font-bold text-center'>ALL BUYERS</h2>
@@ -39,8 +65,9 @@ const AllBuyers = () => {
                                 <th>{[i + 1]}</th>
                                 <td>{buyer.name}</td>
                                 <td>{buyer.email}</td>
-                                <td><button className='btn btn-success'>Pay</button></td>
-                                <td><button className='btn btn-error'>Delete</button></td>
+                                <td><button className='btn btn-warning btn-sm'>Verify</button></td>
+                                <td><button className='btn btn-success btn-sm'>Make Admin</button></td>
+                                <td><button className='btn btn-error btn-sm' onClick={() => handleDelete(buyer._id)}>Delete</button></td>
                             </tr>
 
                             )
--- src/components/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/components/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import { toast, Toaster } from 'react-hot-toast';
 
 const AllSellers = () => {
     const [data, setData] = useState([]);
+    const [reFetch, setReFetch] = useState(false);
 
     useEffect(() => {
         getSellers();
-    }, []);
+    }, [reFetch]);
+
+
     //using axios to get data from api
     const getSellers = () => {
         axios.get('http://localhost:5000/sellers')
@@ -16,6 +20,29 @@ const AllSellers = () => {
             })
             .catch(error => console.log(error));
     }
+
+
+    const handleDelete = (id) => {
+        const doDelete = window.confirm('Do you want to delete this seller?');
+        if(doDelete){
+            fetch(`http://localhost:5000/sellers/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.deletedCount === 1){
+                    setReFetch(!reFetch)
+                   toast.error("Deleted Successfully")
+                }
+            })
+        }
+    }
+
+
     return (
         <div className='p-5 sm:p-5 md:p-8 lg:p-14 my-5'>
             <h2 className='text-2xl my-4 font-bold text-center'>ALL SELLERS</h2>
@@ -39,16 +66,18 @@ const AllSellers = () => {
                                 <th>{[i + 1]}</th>
                                 <td>{seller.name}</td>
                                 <td>{seller.email}</td>
-                                <td><button className='btn btn-success'>Pay</button></td>
-                                <td><button className='btn btn-error'>Delete</button></td>
+                                <td><button className='btn btn-warning btn-sm'>Verify</button></td>
+                                <td><button className='btn btn-success btn-sm'>Make Admin</button></td>
+                                <td><button className='btn btn-error btn-sm' onClick={() => handleDelete(seller._id)}>Delete</button></td>
                             </tr>
 
                             )
                         }
                     </tbody>
                 </table>
-
+                
             </div>
+            <Toaster></Toaster>
         </div>
     );
 
