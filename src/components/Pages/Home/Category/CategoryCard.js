c84e72f4d12477965e8a1829ec0ab44110c9bca6
ajkn1904 <ajknishat@gmail.com>
2023-08-08 21:16:27 +0600

    updated

--- src/components/Pages/Home/Category/CategoryCard.js
+++ src/components/Pages/Home/Category/CategoryCard.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const CategoryCard = ({item}) => {
+const CategoryCard = ({ item }) => {
     return (
-        <div className={`card w-[80%] mx-auto shadow-xl {
-            ${item.name === "Story"} && bg-[url('https://i.ibb.co/y4fr6n2/1000-F-556497357-IHrxvr6-A648b-QUTnlm-Zd-NQi-SPj95tp5y.jpg')]
-        }`}>
+        <div className='w-[80%] mx-auto hover:shadow-lg hover:shadow-green-700 rounded-2xl'>
+            <Link to={`/category/${item._id}`} className={`card h-56 bg-cover bg-slate-900`} style={{
+            backgroundImage: `url(${item.cat_img})`,
+        }}>
             <div className="card-body">
-                <h2 className="card-title text-white">{item.name}</h2>
+                <h2 className="card-title bg-slate-900 text-white p-1 uppercase">{item.name}</h2>
                 <div className="card-actions justify-end">
-                <button className="btn btn-success my-8 mx-auto"><Link to={`/category/${item._id}`}>View Details</Link></button>
                 </div>
             </div>
+            <button className="my-2 mx-auto text-white"><Link to={`/category/${item._id}`}>Click to View</Link></button>
+        </Link>
         </div>
     );
 };
